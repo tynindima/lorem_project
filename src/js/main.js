6f56cb4dbfc150de1b766f50b5b3278ce3935e63
tynindima <tynindima@gmail.com>
2020-09-07 22:08:28 +0300

    Added body-sort

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,17 @@ $(document).ready(() => {
     $(`#header-navbarNav`).collapse(`toggle`);
   });
 
+  for (let i = 1; i <= 5; i++) {
+    $(`#sort-bd-${i}`).on(`click`, () => {
+      $(`.sort-bd__item`).removeClass(`active`);
+      $(`#sort-bd-${i}`).addClass(`active`);
+    });
+  }
+
+  $(`#body-sort-btn`).on(`click`, () => {
+    $(`#body-sortbar`).collapse(`toggle`);
+  });
+
   // eslint-disable-next-line no-console
   console.log(`document ready`);
 });
